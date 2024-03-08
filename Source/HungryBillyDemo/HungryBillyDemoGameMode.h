d0c942101bffabd8f2e8c8cffe351b13da490e28
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2024-03-08 13:34:44 +0000

    Repo Initialisation
    Initialised GitHub repo and created UE5 project using Third Person template.

---
+++ Source/HungryBillyDemo/HungryBillyDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HungryBillyDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHungryBillyDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHungryBillyDemoGameMode();
+};
+
+
+
